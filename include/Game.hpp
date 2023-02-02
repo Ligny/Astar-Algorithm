767b49991b93db7a1eb94ab0d0c4acaa3739e068
Ligny <antoine.deligny@epitech.eu>
2023-02-02 22:32:51 +0900

    ADD Game.hpp

---
+++ include/Game.hpp
@@ -0,0 +1,35 @@
+/**
+ * @ Author: Antoine Deligny
+ * @ Create Time: 2023-01-28 14:17:30
+ * @ Description:
+ */
+
+#pragma once
+
+#include "iostream"
+#include "string"
+
+#include <SFML/Graphics.hpp>
+
+
+#include "Map.hpp"
+#include "Astar.hpp"
+
+class Game
+{
+    public:
+        Game(int width, int height, const std::string& title, std::uint16_t tileSize);
+
+        void run(sf::Vector2f start, sf::Vector2f target);
+    
+    private:
+        void display();
+        void event();
+
+        void mouseEvent();
+        void keyEvent(sf::Keyboard::Key& key);
+
+        sf::RenderWindow m_window;
+        Map m_map;
+        Astar m_astar;
+};
