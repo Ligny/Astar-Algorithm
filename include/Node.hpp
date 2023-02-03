07a43190a4557e66f87b482ae6426e2d23f91527
Ligny <antoine.deligny@epitech.eu>
2023-02-03 17:31:16 +0900

    ADD Node.hpp Astar + rawMap Astar implementation without SFML

--- include/Node.hpp
+++ include/Node.hpp
@@ -13,21 +13,31 @@ template<typename T, typename V>
 class Node
 {
     public:
-        Node(V pos, T& w);
-        Node(const Node& other);
-        Node(Node&& other);
+        Node(T map, V pos) : m_map(map), m_pos(pos) {};
+        ~Node() = default;
 
-        Node& operator=(Node&& other);
-        Node& operator=(const Node& other);
+        void setParent(std::shared_ptr<Node<T, V>> parent);
+        void setGCost(int gCost);
+        void setHCost(int hCost);
+        void setFCost(int fCost);
+        void setPos(V pos);
+        void setMap(T map);
 
-        float m_f;
-        float m_g;
-        float m_h;
+        std::shared_ptr<Node<T, V>> getParent() const;
+        int getGCost() const;
+        int getHCost() const;
+        int getFCost() const;
+        V getPos() const;
+        T getMap() const;
 
-        V m_pos;
+        bool operator==(const Node<T, V>& other) const;
+        bool operator!=(const Node<T, V>& other) const;
 
     private:
-        
-        
-        std::shared_ptr<Node> m_parent;
+        std::shared_ptr<Node<T, V>> m_parent;
+        int m_gCost;
+        int m_hCost;
+        int m_fCost;
+        V m_pos;
+        T m_map;
 };
