59ce934f6c587888f10847130e2b7983d7775c7c
Ligny <antoine.deligny@epitech.eu>
2023-02-08 23:37:29 +0900

    ADD BUTTON component

---
+++ include/ActivateText.hpp
@@ -0,0 +1,27 @@
+/**
+ * @ Author: Antoine Deligny
+ * @ Create Time: 2023-02-07 18:40:22
+ * @ Description:
+ */
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+
+class ActivateText : public sf::Drawable, public sf::Transformable
+{
+    public:
+        explicit ActivateText(sf::Text text, bool active);
+        ActivateText() = default;
+
+        void setActive(bool active);
+
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    private:
+        sf::Text m_text;
+        bool m_active;
+        sf::Font m_font;
+        sf::RectangleShape m_rect;
+};
