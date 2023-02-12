9185746724119c3df8c7ac767be45f9f52191790
Ligny <antoine.deligny@epitech.eu>
2023-02-12 19:53:36 +0900

    ADD Text && Button

--- src/SFML/Game.cpp
+++ src/SFML/Game.cpp
@@ -21,8 +21,17 @@ void Game::setUp(sf::Vector2f start, sf::Vector2f target)
     if (!m_font.loadFromFile("Sansation.ttf"))
         std::cout << "Error loading font" << std::endl;
     sf::Text temp_text = sf::Text(" - Diagonal", m_font);
-    temp_text.setPosition((m_map.getWidth() * m_map.getTileSize()) + 100, 70);
+    temp_text.setPosition((m_map.getWidth() * m_map.getTileSize()) + 75, 70);
+
+    sf::Text temp_text1 = sf::Text(" - Start", m_font);
+    temp_text1.setPosition((m_map.getWidth() * m_map.getTileSize()) + 75, 210);
+
+    sf::Text temp_text2 = sf::Text(" - Reset", m_font);
+    temp_text2.setPosition((m_map.getWidth() * m_map.getTileSize()) + 75, 350);
+
     m_activateTexts.push_back(ActivateText(temp_text, false));
+    m_activateTexts.push_back(ActivateText(temp_text1, false));
+    m_activateTexts.push_back(ActivateText(temp_text2, false));
 }
 
 void Game::run(sf::Vector2f start, sf::Vector2f target)
@@ -38,7 +47,18 @@ void Game::display()
 {
     m_window.clear();
     m_window.draw(m_map);
-    m_window.draw(m_activateTexts[0]);
+    std::string pos = "Start\nx: " + std::to_string((int)m_map.getStart().getPosition().x / m_map.getTileSize()) + " y: " + std::to_string((int)m_map.getStart().getPosition().y / m_map.getTileSize());
+    sf::Text tot(pos, m_font);
+    tot.setFillColor(sf::Color::Green);
+    tot.setPosition((m_map.getWidth() * m_map.getTileSize()) + 30, 500);
+    std::string pos1 = "Target\nx: " + std::to_string((int)m_map.getTarget().getPosition().x / m_map.getTileSize()) + " y: " + std::to_string((int)m_map.getTarget().getPosition().y / m_map.getTileSize());
+    sf::Text tat(pos1, m_font);
+    tat.setFillColor(sf::Color::Red);
+    tat.setPosition((m_map.getWidth() * m_map.getTileSize()) + 30, 600);
+    m_window.draw(tot);
+    m_window.draw(tat);
+    for (auto& text : m_activateTexts)
+        m_window.draw(text);
     m_window.display();
 }
 
@@ -64,12 +84,10 @@ void Game::mouseEvent()
 
 std::vector<std::vector<Node>> getProtoMap(const int width, const int height, const int tileSize)
 {
-    auto rHeight = height / tileSize;
-    auto rWidth = width / tileSize;
     std::vector<std::vector<Node>> map;
-    for (auto y = 0; y < rHeight; y++) {
+    for (auto y = 0; y < height; y++) {
         std::vector<Node> row;
-        for (auto x = 0; x < rWidth; x++) {
+        for (auto x = 0; x < width; x++) {
             row.push_back(Node({x, y}, {-1, -1}, __FLT_MAX__, true));
         }
         map.push_back(row);
@@ -87,7 +105,6 @@ void Game::keyEvent(sf::Keyboard::Key& key)
     } else if (key == sf::Keyboard::T && m_map.isInMap(mousePosition)) {
         m_map.setTarget(mousePosition, sf::Color::Red);
     } else if (key == sf::Keyboard::Enter) {
-        std::cout << m_map.getStart().getPosition().x / m_map.getTileSize() << " " << m_map.getStart().getPosition().y / m_map.getTileSize() << std::endl;
         Astar astar(
             getProtoMap(m_map.getWidth(), m_map.getHeight(), m_map.getTileSize()),
             {m_map.getStart().getPosition().x / m_map.getTileSize(), m_map.getStart().getPosition().y / m_map.getTileSize()},
