7dbf531c87016fca8ae3696e8adbd19cddaf1c53
Ligny <antoine.deligny@epitech.eu>
2023-02-15 16:40:02 +0900

    ADD arg handling generate the size  of the map + error_handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,28 @@
 
 #include "../include/Game.hpp"
 
+std::pair<int, int> error_handling(int arg1, int arg2)
+{
+    if (arg1 == 0 || arg2 == 0)
+        throw;
+    return std::pair<int, int>(arg1, arg2);
+}
+
 int main(int argc, char *argv[])
 {
     try {
-        Game game(32, 18, "Astar", 40);
-        game.run({0, 0}, {0, 3});
+        if (argc == 1) {
+            Game game(32, 18, "Astar", 40);
+            game.run({0, 0}, {12, 15});
+        } else if (argc == 3) {
+            auto args = error_handling(atoi(argv[1]), atoi(argv[2]));
+            Game game(args.first, args.second, "Astar", 40);
+            game.run({0, 0}, {1, 0});
+        } else
+            throw;
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
+        std::cout << "ERROR: Please read the README" << std::endl;
         return EXIT_FAILURE;
     }
     return 0;
